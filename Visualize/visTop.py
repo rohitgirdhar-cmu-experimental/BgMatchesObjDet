b25568e7101b0484da1992dd8e8b7b76d61cdc8c
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2015-01-12 23:40:12 -0500

    gen the query image

--- Visualize/visTop.py
+++ Visualize/visTop.py
@@ -11,15 +11,21 @@ def main():
     with open("../dataset/PeopleAtLandmarks/ImgsList.txt") as f:
         lst = f.read().splitlines()
     for i in range(1, 50):
-        I = cv2.imread(imgsdir + lst[i])
+        out_dpath = "../tempdata/tops_vis/" + str(i) + "/"
+        I = cv2.imread(imgsdir + lst[i - 1])
+        with open("../tempdata/marked_boxes/" + str(i) + ".txt") as fid:
+            box = fid.readline().strip().split(',')
+        qbox = [int(float(el)) for el in box]
+        cv2.rectangle(I, (qbox[1], qbox[0]), (qbox[3], qbox[2]), (0,255,0))
+        cv2.imwrite(out_dpath + "q.jpg", I)
 
         topimgs, bboxes = readTopList("../tempdata/tops/" + str(i) + ".txt")
         j = 0
-        out_dpath = "../tempdata/tops_vis/" + str(i) + "/"
         if not os.path.exists(out_dpath):
             os.makedirs(out_dpath)
         for topimg in topimgs:
-            J = cv2.imread(imgsdir + lst[topimg])
+            J = cv2.imread(imgsdir + lst[topimg - 1])
+            # bbox are in sel search format 
             cv2.rectangle(J, (int(bboxes[j][1]), int(bboxes[j][0])), 
                     (int(bboxes[j][3]), int(bboxes[j][2])), (0,0,255))
             cv2.imwrite(out_dpath + str(j) + ".jpg", J)
