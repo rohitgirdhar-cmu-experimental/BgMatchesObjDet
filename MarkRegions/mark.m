f7bb8c34899ed2abd5302725511e09321f71da1e
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2015-01-13 00:08:52 -0500

    dedup

--- MarkRegions/mark.m
+++ MarkRegions/mark.m
@@ -6,10 +6,15 @@ fclose(fid);
 
 fig = figure;
 for i = 1 : numel(lst)
+    outf = fullfile(outdir, [num2str(i) '.txt']);
+    if exist(outf, 'file')
+        fprintf(2, 'Already done for %d\n', i);
+        continue;
+    end
+
     I = imread(fullfile(imgsdir, lst{i}));
     imshow(I);
     rect = getrect(fig);
-    outf = fullfile(outdir, [num2str(i) '.txt']);
     fid = fopen(outf, 'w');
     % write in Selective Search format of y1,x1,y2,x2
     fprintf(fid, '%f,%f,%f,%f\n', rect(2), rect(1), rect(4) + rect(2), rect(3) + rect(1));
