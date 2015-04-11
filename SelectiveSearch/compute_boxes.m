c5e789d9ea343dbac438f5f45bfda6a45c5ef019
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2015-04-10 21:28:13 -0400

    compute boxes update

--- SelectiveSearch/compute_boxes.m
+++ SelectiveSearch/compute_boxes.m
@@ -1,5 +1,5 @@
-function compute_boxes(imgsDir, imgsList)
-% Compute the selective search boxes for each image in the dpath and store into a mat
+function compute_boxes(imgsDir, imgsList, outdir)
+% Compute the selective search boxes for each image in the dpath and store into a txt
 
 addpath(genpath('SelectiveSearchCodeIJCV'));
 
@@ -8,25 +8,43 @@ lst = textscan(fid, '%s\n');
 lst = lst{1};
 fclose(fid);
 
+if ~exist(outdir, 'dir')
+  mkdir(outdir);
+end
+
 try
-    matlabpool open 6;
+    matlabpool open 16;
 catch
 end
 parfor i = 1 : numel(lst)
+    outpath = fullfile(outdir, [num2str(i) '.txt']);
+    lockpath = [outpath '.lock'];
+
+    if exist(lockpath, 'dir') || exist(outpath, 'file')
+      continue;
+    end
+    unix(['mkdir -p ' lockpath]);
+
     impath_str = lst{i};
     I = imread(fullfile(imgsDir, impath_str));
-    boxes{i} = selective_search_boxes(I, true, 512);
+    boxes = selective_search_boxes(I, true, 480);
+
+    % remove too small boxes
+    boxes2 = zeros(0, 4);
+    for j = 1 : size(boxes, 1)
+      if (boxes(j, 3) - boxes(j, 1)) < 25 || ...
+          (boxes(j, 4) - boxes(j, 2)) < 25
+          continue;
+      end
+      boxes2(end+1, :) = boxes(j, :);
+    end
+
+    saveBoxes(boxes2, outpath);
+
+    unix(['rmdir ' lockpath]);
     i
 end
 
-disp('Saving to disk');
-mkdir('../tempdata');
-%save('../tempdata/selsearch_boxes.mat', 'boxes');
-saveTxt('../tempdata/selsearch_boxes/', boxes);
-
-function saveTxt(dpath, boxes)
-mkdir(dpath);
-for i = 1 : numel(boxes)
-    dlmwrite(fullfile(dpath, [num2str(i) '.txt']), boxes{i});
-end
+function saveBoxes(boxes, fpath)
+dlmwrite(fpath, boxes);
 
